0cbd9ab5b75cd51bd8e994d6e4dd974f43c706d0
nandini-menon <menonnandini98@gmail.com>
2020-06-25 15:13:40 +0530

    Put code for making predictions into functions

--- src/neural_network/make_prediction.py
+++ src/neural_network/make_prediction.py
@@ -1,35 +1,43 @@
-import random
-import numpy as np
 import pandas as pd
 from tensorflow import keras
-# from google.colab import drive
 
-# drive.mount('/content/gdrive')
-# root_path = 'gdrive/My Drive/MainProject/'
 
-root_path = '../../data/'
+def sample_dataset():
+    root_path = '../../data/'
+    df = pd.read_csv(f'{root_path}test_set.csv')
 
-new_model = keras.models.load_model(f'model_3/model_3.h5')
-filename = f'{root_path}test_set.csv'
-df = pd.read_csv(filename)
+    df_0 = df[df['Label'] == 0]
+    df_1 = df[df['Label'] == 1]
 
-df_0 = df[df['Label'] == 0]
-df_1 = df[df['Label'] == 1]
+    df_0 = df_0.sample(n=15)
+    df_1 = df_1.sample(n=5)
 
-df_0 = df_0.sample(n=15)
-df_1 = df_1.sample(n=5)
+    df = pd.concat([df_0, df_1], sort=True)
+    df = df.sample(frac=1)
 
-df = pd.concat([df_0, df_1], sort=True)
-df = df.sample(frac=1)
+    dataset = df.values
+    X_new = dataset[:, 0:21].astype(float)
+    Y_new = dataset[:, 21].astype("int32")
 
-dataset = df.values
-X_new = dataset[:, 0:21].astype(float)
-Y_new = dataset[:, 21].astype("int32")
+    return (X_new, Y_new)
 
-result = (new_model.predict(X_new) > 0.5).astype("int32")
-print("Prediction\t Actual Result")
-print("------------------------------")
-for i in range(len(Y_new)):
-    print(f'{result[i][0]}\t\t\t     {Y_new[i]}')
-_, acc = new_model.evaluate(X_new, Y_new)
-print('Accuracy: {:5.2f}%'.format(100*acc))
+
+def get_predictions(X_new, Y_new):
+    trained_model = keras.models.load_model(f'model_3/model_3.h5')
+    result = (trained_model.predict(X_new) > 0.5).astype("int32")
+
+    print("Prediction\t Actual Result")
+    print("------------------------------")
+    for i in range(len(Y_new)):
+        print(f'{result[i][0]}\t\t\t     {Y_new[i]}')
+
+    _, acc = trained_model.evaluate(X_new, Y_new)
+    print('Accuracy: {:5.2f}%'.format(100*acc))
+
+
+def main():
+    X_new, Y_new = sample_dataset()
+    get_predictions(X_new, Y_new)
+
+if __name__ == '__main__':
+    main()
