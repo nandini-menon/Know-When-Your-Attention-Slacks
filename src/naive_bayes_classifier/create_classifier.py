c00071a816028af0f569d3465584130b2d00a320
nandini-menon <menonnandini98@gmail.com>
2020-06-26 12:29:21 +0530

    Add naive bayes classifier

---
+++ src/naive_bayes_classifier/create_classifier.py
@@ -0,0 +1,42 @@
+import pickle
+from pandas import read_csv
+from sklearn.naive_bayes import GaussianNB
+from sklearn import metrics
+
+
+def get_data():
+    root_path = '../../data/'
+    train_df = read_csv(f'{root_path}training_set.csv')
+    test_df = read_csv(f'{root_path}test_set.csv')
+
+    train_dataset = train_df.values
+    test_dataset = test_df.values
+
+    X_train = train_dataset[:, 0:21].astype(float)
+    Y_train = train_dataset[:, 21].astype('int32')
+
+    X_test = test_dataset[:, 0:21].astype(float)
+    Y_test = test_dataset[:, 21].astype('int32')
+
+    return (X_train, Y_train, X_test, Y_test)
+
+
+def create_classifier(X_train, Y_train):
+    gnb = GaussianNB()
+    gnb.fit(X_train, Y_train)
+    pickle.dump(gnb, open('gnb_model.sav', 'wb'))
+
+
+def evaluate_classifier(X_test, Y_test):
+    model = pickle.load(open('gnb_model.sav', 'rb'))
+    Y_pred = model.predict(X_test)
+    print("Accuracy:",metrics.accuracy_score(Y_test, Y_pred))
+
+
+def main():
+    X_train, Y_train, X_test, Y_test = get_data()
+    create_classifier(X_train, Y_train)
+    evaluate_classifier(X_test, Y_test)
+
+if __name__ == '__main__':
+    main()
---
+++ src/naive_bayes_classifier/make_predictions.py
@@ -0,0 +1,35 @@
+import pickle
+import pandas as pd
+from sklearn import metrics
+
+
+def sample_dataset():
+    root_path = '../../data/'
+    df = pd.read_csv(f'{root_path}test_set.csv')
+    df = df.sample(n=20)
+    dataset = df.values
+
+    X_new = dataset[:, 0:21].astype(float)
+    Y_new = dataset[:, 21].astype("int32")
+
+    return (X_new, Y_new)
+
+
+def get_predictions(X_new, Y_new):
+    model = pickle.load(open('gnb_model.sav', 'rb'))
+    Y_pred = model.predict(X_new)
+
+    print("Prediction\t Actual Result")
+    print("------------------------------")
+    for i in range(len(Y_new)):
+        print(f'{Y_pred[i]}\t\t\t     {Y_new[i]}')
+
+    print("Accuracy:", metrics.accuracy_score(Y_new, Y_pred))
+
+
+def main():
+    X_new, Y_new = sample_dataset()
+    get_predictions(X_new, Y_new)
+
+if __name__ == '__main__':
+    main()
